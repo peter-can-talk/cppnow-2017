fbca8c2d49e188ef306ea5f5aa168a8650d14b1d
goldsborough <peter@goldsborough.me>
2017-05-19 16:27:26 -0600

    Fixed up using-vs-typedef

--- code/using-vs-typedef/using.cpp
+++ code/using-vs-typedef/using.cpp
@@ -13,40 +13,41 @@
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/raw_ostream.h>
 
-// Standard includes
-#include <cassert>
-
 namespace UsingTool {
 
+/// Acts on each `typedef` by emitting a diagnostic and FixItHint.
 class MatchHandler : public clang::ast_matchers::MatchFinder::MatchCallback {
  public:
   using MatchResult = clang::ast_matchers::MatchFinder::MatchResult;
 
+  /// Warns about the use of `typedef` and recommends `using` via a `FixItHint`.
   void run(const MatchResult& Result) {
     const auto* Typedef = Result.Nodes.getNodeAs<clang::TypedefDecl>("typedef");
-    assert(Typedef != nullptr);
 
-    auto& Diagnostics = Result.Context->getDiagnostics();
+    clang::DiagnosticsEngine& Diagnostics = Result.Context->getDiagnostics();
     const unsigned ID =
         Diagnostics.getCustomDiagID(clang::DiagnosticsEngine::Warning,
                                     "Prefer 'using' to 'typedef'");
 
-    const auto Start = Typedef->getLocStart();
-    const auto End = Start.getLocWithOffset(+7);
-    const auto FixIt =
-        clang::FixItHint::CreateReplacement({Start, End}, "using");
+    const auto UsingString =
+        (llvm::Twine("using ") + Typedef->getName() + " = ...").str();
+    const clang::SourceRange Range = Typedef->getSourceRange();
+    const auto FixIt = clang::FixItHint::CreateReplacement(Range, UsingString);
 
-    clang::DiagnosticBuilder DB =
-        Diagnostics.Report(Typedef->getLocation(), ID);
-    DB.AddFixItHint(FixIt);
+    // Note: getLocation() points to the start of the typedef'd name,
+    // e.g. `MyInt` in `typedef int MyInt`. So use `getLocStart()` instead.
+    Diagnostics.Report(Typedef->getLocStart(), ID).AddFixItHint(FixIt);
   }
 };
 
+/// Consumes a translation unit by dispatching an `ASTMatcher` on it.
 class Consumer : public clang::ASTConsumer {
  public:
+  /// Creates an `ASTMatcher` and dispatches it on the AST.
   void HandleTranslationUnit(clang::ASTContext& Context) {
     using namespace clang::ast_matchers;
 
+    /// Could also use a RecursiveASTVisitor and VisitTypedefDecl.
     const auto Matcher = typedefDecl(isExpansionInMainFile()).bind("typedef");
 
     MatchHandler Handler;
@@ -56,6 +57,7 @@ class Consumer : public clang::ASTConsumer {
   }
 };
 
+/// Creates an `ASTConsumer`.
 class Action : public clang::ASTFrontendAction {
  public:
   using ASTConsumerPointer = std::unique_ptr<clang::ASTConsumer>;
@@ -69,11 +71,23 @@ class Action : public clang::ASTFrontendAction {
 
 namespace {
 llvm::cl::OptionCategory UsingToolCategory("UsingTool Options");
-
 llvm::cl::extrahelp UsingToolCategoryHelp(R"(
     Verifies that you use `using` instead of `typedef`.
+
+    For example, given this declaration:
+
+    typedef int MyInt;
+
+    This tool will emit
+
+    ...: warning: Prefer 'using' to 'typedef'
+    typedef int MyInt;
+    ~~~~~~~     ^
+    using
 )");
 
+llvm::cl::extrahelp
+    CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 }  // namespace
 
 auto main(int argc, const char* argv[]) -> int {
